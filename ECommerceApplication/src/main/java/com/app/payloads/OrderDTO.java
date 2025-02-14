9f424a3ed78b55960602373514193bc28ca53b91
Septio Nugroho <septio.nugroho@ui.ac.id>
2025-02-14 19:36:02 +0700

    feat: add address field to payment

--- ECommerceApplication/src/main/java/com/app/payloads/OrderDTO.java
+++ ECommerceApplication/src/main/java/com/app/payloads/OrderDTO.java
@@ -3,7 +3,6 @@ package com.app.payloads;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderDTO {
-	
+
 	private Long orderId;
 	private String email;
 	private List<OrderItemDTO> orderItems = new ArrayList<>();
@@ -20,5 +19,5 @@ public class OrderDTO {
 	private PaymentDTO payment;
 	private Double totalAmount;
 	private String orderStatus;
-
+	private AddressDTO address;
 }
